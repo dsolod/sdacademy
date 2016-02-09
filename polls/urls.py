e7be6ccdbdc3e57e9add046f1f73fec1c2135f0c
denys.solodov <denys.solodov@oracle.com>
2016-02-09 18:38:40 +0200

    tut3-4

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,8 @@ urlpatterns = patterns('',
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    #url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^specifics/(?P<question_id>\d+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 #from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
 from polls.models import Question
 
