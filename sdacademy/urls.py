f367a85df1179b53e3755e495f9114af9aeb936e
denys.solodov <denys.solodov@oracle.com>
2016-02-11 05:53:42 +0200

    fix views and js 8.2

--- sdacademy/urls.py
+++ sdacademy/urls.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from sdacademy import views
 from django.conf.urls.static import static
+#from views import contact, student_list, index, student_detail
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index),
-    url(r'^index/$', views.index),
-    url(r'^contact/$', views.contact),
-    url(r'^student_list/$', views.student_list),
-    url(r'^student_detail/$', views.student_detail),
+    url(r'^$', 'sdacademy.views.index',name='index'),
+    url(r'^index/$', 'sdacademy.views.index',name='index'),
+    url(r'^contact/', 'sdacademy.views.contact', name='contact'),
+    url(r'^student_list/', 'sdacademy.views.student_list', name='student_list'),
+    url(r'^student_detail/', 'sdacademy.views.student_detail', name='student_detail'),
+
 
 
 
