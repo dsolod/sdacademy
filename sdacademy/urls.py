538df716988456c4704f5bc25903b64073dd9848
denys.solodov <denys.solodov@oracle.com>
2016-02-11 04:44:30 +0200

    8.2 start

--- sdacademy/urls.py
+++ sdacademy/urls.py
@@ -1,8 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from sdacademy import views
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
+    url(r'^index/$', views.index),
+    url(r'^contact/$', views.contact),
+    url(r'^student_list/$', views.student_list),
+    url(r'^student_detail/$', views.student_detail),
+
+
+
 )
 
