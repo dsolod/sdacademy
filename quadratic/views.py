345e67a74287700b0943cb00ee571f07e56606bb
denys.solodov <denys.solodov@oracle.com>
2016-03-09 23:11:24 +0200

    fix some issue with quadratic

--- quadratic/views.py
+++ quadratic/views.py
@@ -6,29 +6,24 @@ from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext
 from .forms import QuadraticForm
 
-def quadratic_results(request):
-    
-    context = {}
-    answers = ('Дискриминант меньше нуля, квадратное уравнение не \
-        имеет действительных решений.', 'Дискриминант равен нулю, квадратное \
-        уравнение имеет один действительный корень: x1 = x2 = ', 'Квадратное \
-        уравнение имеет два действительных корня: x1 = ')
 
+def quadratic_results(request):
+
+    context = {}
+    answers = (
+'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.', 
+'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = ', 
+'Квадратное уравнение имеет два действительных корня: x1 = ')
 
     diskriminant = ""
     text_diskriminant = ""
     if request.method == 'GET' and request.GET:
-
         form = QuadraticForm(request.GET)
-        
         if form.is_valid():
             form.clean_a()
-
             a = form.cleaned_data['a']
             b = form.cleaned_data['b']
             c = form.cleaned_data['c']
-
-
             diskriminant = b*b - 4*a*c
             if diskriminant < 0:
                 text_diskriminant = answers[0]
@@ -39,19 +34,16 @@ def quadratic_results(request):
             if diskriminant > 0:
                 x1 = (-b + math.sqrt(diskriminant))/(2.0*a)
                 x2 = (-b - math.sqrt(diskriminant))/(2.0*a)
-                text_diskriminant = answers[2]+ str(round(x1, 1)) +', x2 = '+ str(round(x2, 1))
+                text_diskriminant = answers[2] + str(round(x1, 1)) + (
+                    ', x2 = ' + str(round(x2, 1))
+                    )
                 #print x1
     else:
         form = QuadraticForm()
 
-
     return render(request, 'quadratic/results.html', 
         {
-
-
-                'diskriminant' : diskriminant,
-                'text_diskriminant' : text_diskriminant,
-                'form' : form
+                'diskriminant': diskriminant,
+                'text_diskriminant': text_diskriminant,
+                'form': form
         })
-
-
