4d45b73bdc5dc065dceba429f67bc6312ba11739
denys.solodov <denys.solodov@oracle.com>
2016-03-10 06:01:48 +0200

    12.1

--- students/urls.py
+++ students/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import patterns, url
 from students import views
 
 urlpatterns = patterns('',
-#	url(r'^(?P<course_id>=\d+)/$', views.IndexView.as_view(), name='index'),
-#    url(r'^$', views.IndexView.as_view(), name='list'),
+
     url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^add/', views.create, name='create'),
+    url(r'^edit/(?P<pk>\d+)', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>\d+)', views.remove, name='remove'),
     url(r'^(?P<course_id>=\d+)/$', views.list, name='list'),
     url(r'^$', views.list, name='list'),
-#    url(r'^(?P<pk>\d+)', views.DetailView.as_view(), name='detail'),
+
 
 
 
--- students/views.py
+++ students/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
 from students.models import Student
 from courses.models import Course
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from .forms import StudentModelForm
+from django.contrib import messages
 '''
 class IndexView(generic.ListView):
     template_name = '../templates/students/list.html'
     context_object_name = 'students_list'
-  
     def get_queryset(self):
         try:
             cour= self.request.GET['course_id']
@@ -17,31 +18,70 @@ class IndexView(generic.ListView):
 
             return Student.objects.all()
 '''
+
+
 def list(request):
     try:
-        cour= request.GET['course_id']
-        
+        cour = request.GET['course_id']
         students = Student.objects.filter(courses__in=cour)
     except:
         students = Student.objects.all()
-   
-        
-
     return render(request, '../templates/students/list.html', 
-        { 'students_list': students })
+        {'students_list': students})
 
 
 def detail(request, pk):
-    
     students = Student.objects.get(id=pk)
-
     return render(request, '../templates/students/detail.html', 
-        {  'student': students })
+        {'student': students})
+
+
+def create(request):
+    if request.method == "POST":
+        form = StudentModelForm(request.POST)
+        #print request.POST
+        if form.is_valid():
+
+            data = form.cleaned_data
+            messages.success(request, 'Student ' + data['name'] +
+             ' ' + data['surname'] + ' has been successfully added.')
+            form.save()
+    else:
+        form = StudentModelForm()
+    return render(request, '../templates/students/add.html', 
+        {'form': form})
+
+
+def edit(request, pk):
+    app = Student.objects.get(id=pk)
+    if request.method == "POST":
+        form = StudentModelForm(request.POST, instance=app)
+        if form.is_valid():
+
+            messages.success(request, 'Info on the student has been sucessfully changed.')
+            app = form.save()
+    else:
+        form = StudentModelForm(instance=app)
+
+    return render(request, '../templates/students/edit.html', {'form': form})
+
+
+def remove(request, pk):
+    app = Student.objects.get(id=pk)
+    messages.info(request, 'Student ' + app.name + ' ' + app.surname +
+     ' will be deleted.')
+    if request.method == "POST":
+        messages.success(request, 'Info on ' + app.name + ' ' + app.surname +
+         ' has been successfully deleted.')
+
+        app.delete()
+        return redirect('students:list')
+
+    return render(request, '../templates/students/remove.html', {'app': app})
+
+
 '''
 class DetailView(generic.DetailView):
     model = Student
     template_name = '../templates/students/detail.html'
 '''
-
-
-
