cf54d10032e57bd95225929de819247a57ff58c2
denys.solodov <denys.solodov@oracle.com>
2016-03-10 20:05:17 +0200

    12.2

--- courses/urls.py
+++ courses/urls.py
@@ -9,7 +9,11 @@ from courses import views
 urlpatterns = patterns('',
     url(r'^$', views.courses_list, name='courses_list'),
    # url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^add/', views.add, name='add'),
+    url(r'^edit/(?P<pk>\d+)/', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>\d+)/', views.remove, name='remove'),
     url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>\d+)/add-lesson/', views.add_lesson, name='add_lesson'),
 #    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 
 )
--- courses/views.py
+++ courses/views.py
@@ -1,20 +1,16 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
 from courses.models import Course, Lesson
 from coaches.models import Coach
+from .forms import CourseModelForm, LessonModelForm
+from django.contrib import messages
 
-'''
-class IndexView(generic.ListView):
-    template_name = '../templates/index.html'
-    context_object_name = 'courses_list'
-
-    def get_queryset(self):
-        return Course.objects.all()'''
 
 def courses_list(request):
     courses = Course.objects.all()
     return render(request, '../templates/index.html', 
         {  'courses_list': courses })
+
 
 def detail(request, pk):
     
@@ -31,8 +27,62 @@ def detail(request, pk):
             'assistant' : assistant
          })
 
-'''   
-class DetailView(generic.DetailView):
-    model = Course
-    template_name = '../templates/courses/detail.html'
-'''
+
+def add(request):
+    if request.method == "POST":
+        form = CourseModelForm(request.POST)
+        #print request.POST
+        if form.is_valid():
+
+            data = form.cleaned_data
+            messages.success(request, 'Course ' + data['name'] +
+             ' has been successfully added.')
+            form.save()
+            return redirect('index')
+    else:
+        form = CourseModelForm()
+        return render(request, '../templates/courses/add.html', 
+        {'form': form})
+
+
+def edit(request, pk):
+    app = Course.objects.get(id=pk)
+    if request.method == "POST":
+        form = CourseModelForm(request.POST, instance=app)
+        if form.is_valid():
+            messages.success(request, 'The changes have been saved.')
+            app = form.save()
+    else:
+        form = CourseModelForm(instance=app)
+
+    return render(request, '../templates/courses/edit.html', {'form': form})
+
+
+def remove(request, pk):
+    app = Course.objects.get(id=pk)
+    messages.info(request, 'Course ' + app.name + ' will be deleted.')
+    if request.method == "POST":
+        messages.success(request, 'Course ' + app.name + 
+         ' has been deleted.')
+
+        app.delete()
+        return redirect('courses:courses_list')
+    else:
+        return render(request, '../templates/courses/remove.html', {'app': app})
+
+
+def add_lesson(request, pk):
+    if request.method == "POST":
+        form = LessonModelForm(request.POST)
+        #print request.POST
+        if form.is_valid():
+
+            data = form.cleaned_data
+            messages.success(request, 'Lesson ' + data['subject'] +
+             ' has been successfully added.')
+            form.save()
+            return redirect('courses:detail', pk )
+    else:
+        form = LessonModelForm(initial={'course': pk})
+        return render(request, '../templates/courses/add_lesson.html', 
+        {'form': form})
